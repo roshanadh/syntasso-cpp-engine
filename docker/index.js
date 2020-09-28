8c3ae56aa14a358cbf63002d57063c833f1d2114
Roshan <nahsorad@gmail.com>
2020-09-28 15:11:35 +0545

    Export funcs to build, remove, create, and start

---
+++ docker/index.js
@@ -0,0 +1,6 @@
+module.exports = {
+	buildCppImage: require("./buildCppImage.js"),
+	removeCppContainer: require("./removeCppContainer.js"),
+	createCppContainer: require("./createCppContainer.js"),
+	startCppContainer: require("./startCppContainer.js"),
+};
