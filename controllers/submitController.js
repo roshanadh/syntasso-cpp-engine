b6914f191e375fdf905a43013c562064f1279503
Roshan <nahsorad@gmail.com>
2020-09-30 13:13:45 +0545

    Generate submission file inside client-files before handling dockerConfig

--- controllers/submitController.js
+++ controllers/submitController.js
@@ -3,6 +3,10 @@ const {
 	createCppContainer,
 	startCppContainer,
 } = require("../docker/index.js");
+const {
+	createSubmissionFilePath,
+	generateSubmissionFile,
+} = require("../filesystem/index.js");
 
 const handleConfigZero = (req, res, next) => {
 	buildCppImage(req)
@@ -35,18 +39,28 @@ const handleConfigTwo = (req, res, next) => {};
 
 module.exports = (req, res, next) => {
 	try {
-		const dockerConfig = parseInt(req.body.dockerConfig);
-		switch (dockerConfig) {
-			case 0:
-				handleConfigZero(req, res, next);
-				break;
-			case 1:
-				handleConfigOne(req, res, next);
-				break;
-			case 2:
-				handleConfigTwo(req, res, next);
-				break;
-		}
+		createSubmissionFilePath(req.body.socketId)
+			.then(submissionFilePath => generateSubmissionFile(req))
+			.then(fileName => {
+				{
+					const dockerConfig = parseInt(req.body.dockerConfig);
+					switch (dockerConfig) {
+						case 0:
+							handleConfigZero(req, res, next);
+							break;
+						case 1:
+							handleConfigOne(req, res, next);
+							break;
+						case 2:
+							handleConfigTwo(req, res, next);
+							break;
+					}
+				}
+			})
+			.catch(error => {
+				error.status = 503;
+				next(error);
+			});
 	} catch (error) {
 		error.status = 503;
 		next(error);
