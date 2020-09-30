66badda3ea17a516ede072c6cba581985d72e026
Roshan <nahsorad@gmail.com>
2020-09-30 12:26:33 +0545

    Export client information for cors and update default port to 8082

--- config.js
+++ config.js
@@ -3,5 +3,13 @@ require("dotenv").config();
 if (!process.env) throw new Error("Environment variable(s) not set.");
 
 module.exports = {
-	PORT: process.env.PORT || 8081,
+	PORT: process.env.PORT || 8082,
+	CLIENT_PROTOCOL: process.env.CLIENT_PROTOCOL,
+	CLIENT_HOST: process.env.CLIENT_HOST,
+	CLIENT_PORT: process.env.CLIENT_PORT,
+	SECRET_SESSION_KEY: process.env.SECRET_SESSION_KEY,
+	REDIS_STORE_HOST: process.env.REDIS_STORE_HOST,
+	REDIS_STORE_PORT: process.env.REDIS_STORE_PORT,
+	EXECUTION_TIME_OUT_IN_MS: process.env.EXECUTION_TIME_OUT_IN_MS,
+	MAX_LENGTH_STDOUT: process.env.MAX_LENGTH_STDOUT,
 };
