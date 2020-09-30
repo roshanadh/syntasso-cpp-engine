ef58f42cdefeaff00ccc015ba75520f058a443f0
Roshan <nahsorad@gmail.com>
2020-09-30 11:36:31 +0545

    Use paramValidator middleware before submitController

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-	codeValidator,
-	dockerConfigValidator,
-} = require("../middlewares/paramValidator.js");
+const paramValidator = require("../middlewares/paramValidator.js");
 const submitController = require("../controllers/submitController.js");
 
 router.get("/", (req, res) => {
@@ -14,34 +11,6 @@ router.get("/", (req, res) => {
 	});
 });
 
-router.post(
-	"/submit",
-	(req, res, next) => {
-		console.log("POST request received at /submit");
-
-		if (!codeValidator(req))
-			return res.status(400).json({
-				error: "No code provided",
-			});
-		switch (dockerConfigValidator(req)) {
-			case "no-config":
-				return res.status(400).json({
-					error: "No dockerConfig provided",
-				});
-			case "NaN":
-				return res.status(400).json({
-					error: "dockerConfig should be a number; got NaN",
-				});
-			case "no-valid-config":
-				return res.status(400).json({
-					error: "dockerConfig should be one of [0, 1, 2]",
-				});
-			default:
-				break;
-		}
-		next();
-	},
-	submitController
-);
+router.post("/submit", paramValidator, submitController);
 
 module.exports = router;
