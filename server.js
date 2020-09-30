6d03efe9e2d33f5051fe657bb20cb4b43de4c28d
Roshan <nahsorad@gmail.com>
2020-09-30 11:35:45 +0545

    Use PORT from .env instead of hardcoding

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const { PORT } = require("./config.js");
 const router = require("./routes/router.js");
 
 const app = express();
@@ -8,7 +9,6 @@ app.set("json spaces", 2);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
-const PORT = 8082;
 app.listen(PORT, () =>
 	console.log(`Syntasso C++ Engine is now listening on port ${PORT}...`)
 );
