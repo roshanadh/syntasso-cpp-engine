5d04fa6b9fb99319fa22ceaa48acd9d912aca3e6
Roshan <nahsorad@gmail.com>
2020-09-30 13:14:09 +0545

    Export func to generate .cpp submission file

---
+++ filesystem/generateSubmissionFile.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = req => {
+	const { socketId } = req.body;
+	const fileName = `${socketId}.cpp`;
+	const filePath = path.resolve(
+		__dirname,
+		"..",
+		"client-files",
+		socketId,
+		fileName
+	);
+
+	const { code } = req.body;
+	return new Promise((resolve, reject) => {
+		console.log(`Generating submission file named ${fileName}...`);
+		fs.writeFile(filePath, code, error => {
+			if (error) {
+				console.error(
+					`Error while generating submission file: ${error}`
+				);
+				return reject(error);
+			}
+			console.log(`Submission file ${fileName} generated.`);
+			return resolve(fileName);
+		});
+	});
+};
