bb3d27db173d1a46f160aa8686c7a223551ae3fa
Roshan <nahsorad@gmail.com>
2020-10-02 19:45:47 +0545

    Use socketInstance to emit 'docker-app-stdout' socket event to client

--- docker/createCppContainer.js
+++ docker/createCppContainer.js
@@ -2,14 +2,20 @@ const { exec } = require("child_process");
 
 const removeCppContainer = require("./removeCppContainer.js");
 
-module.exports = req => {
+module.exports = (req, socketInstance) => {
 	return new Promise((resolve, reject) => {
 		try {
-			removeCppContainer(req.body.socketId)
+			const { socketId } = req.body;
+			removeCppContainer(socketId)
 				.then(removalLogs => {
 					console.log("Creating a C++ container...");
+					socketInstance.instance
+						.to(socketId)
+						.emit("docker-app-stdout", {
+							stdout: "Creating a C++ container...",
+						});
 					exec(
-						`docker create -it --name ${req.body.socketId} img_cpp`,
+						`docker create -it --name ${socketId} img_cpp`,
 						(error, stdout, stderr) => {
 							if (error) {
 								console.error(
@@ -26,6 +32,11 @@ module.exports = req => {
 									"stderr while creating C++ container:",
 									stderr
 								);
+								socketInstance.instance
+									.to(socketId)
+									.emit("docker-app-stdout", {
+										stdout: `stderr while creating C++ container: ${stderr}`,
+									});
 								// reject an object with keys error or stderr, because this ...
 								// ... makes it easier to check later if an error occurred ...
 								// ... or an stderr was generated during the build process
@@ -35,6 +46,16 @@ module.exports = req => {
 								`stdout during C++ container creation: ${stdout}`
 							);
 							console.log("C++ container created.");
+							socketInstance.instance
+								.to(socketId)
+								.emit("docker-app-stdout", {
+									stdout: `stdout during C++ container creation: ${stdout}`,
+								});
+							socketInstance.instance
+								.to(socketId)
+								.emit("docker-app-stdout", {
+									stdout: "C++ container created.",
+								});
 							return resolve(stdout);
 						}
 					);
