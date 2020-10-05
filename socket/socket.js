1e200b87cbbaa67709724bc49ee3f848e5943de5
Roshan <nahsorad@gmail.com>
2020-10-05 17:26:25 +0545

    Invoke removeCppContainer and cleanUpClientFiles on socket disconnect

--- socket/socket.js
+++ socket/socket.js
@@ -1,6 +1,7 @@
 const socket = require("socket.io");
 const cryptoRandomString = require("crypto-random-string");
-
+const { removeCppContainer } = require("../docker/index.js");
+const { cleanUpClientFiles } = require("../filesystem/index.js");
 class Socket {
 	constructor(server) {
 		this.instance = socket(server);
@@ -10,17 +11,27 @@ class Socket {
 		this.instance.engine.generateId = () => {
 			return `s-${cryptoRandomString({ length: 18, type: "hex" })}`;
 		};
-
 		this.instance.on("connection", socket => {
 			console.log(
 				`\nCONNECTION: New socket connection with id ${socket.id}\n`
 			);
-
 			socket.on("disconnect", reason => {
 				console.log(
 					`\nDISCONNECT: Socket disconnected with id ${socket.id}`
 				);
 				console.log(`REASON: ${reason}\n`);
+				removeCppContainer(socket.id).catch(error =>
+					console.error(
+						`Error during cleanup after socket ${socket.id} disconnection:`,
+						error
+					)
+				);
+				cleanUpClientFiles(socket.id).catch(error =>
+					console.error(
+						`Error during cleanup after socket ${socket.id} disconnection:`,
+						error
+					)
+				);
 			});
 		});
 	}
