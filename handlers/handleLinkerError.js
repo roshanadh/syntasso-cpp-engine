2e0fd68a6f02df940bc1b39c432af986d8fe3f38
Roshan <nahsorad@gmail.com>
2020-10-07 19:08:10 +0545

    Export func to handle linker errors

---
+++ handlers/handleLinkerError.js
@@ -0,0 +1,18 @@
+const { linkerErrorParser } = require("../util/linkerErrorParser");
+
+module.exports = (req, res, next, linkerError, compilationWarnings) => {
+	try {
+		const _parsedError = linkerErrorParser(error.linkerError);
+		if (_parsedError.errorInParser) return next(_parsedError.errorInParser);
+		const response = {
+			compilationWarnings,
+			error: _parsedError,
+			errorType: "linker-error",
+		};
+		console.log("Response to the client:", response);
+		return res.json(response);
+	} catch (error) {
+		console.error("Error in handleLinkerError:", error);
+		next(error);
+	}
+};
