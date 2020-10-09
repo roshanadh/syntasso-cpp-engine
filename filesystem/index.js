fb6bc3aa902785f742633820d331b0ed3b672f31
Roshan <nahsorad@gmail.com>
2020-10-09 17:48:14 +0545

    Export createTestFilesPath and generateTestFiles

--- filesystem/index.js
+++ filesystem/index.js
@@ -1,5 +1,7 @@
 module.exports = {
 	createSubmissionFilePath: require("./createSubmissionFilePath.js"),
 	generateSubmissionFile: require("./generateSubmissionFile.js"),
+	createTestFilesPath: require("./createTestFilesPath.js"),
+	generateTestFiles: require("./generateTestFiles.js"),
 	cleanUpClientFiles: require("./cleanUpClientFiles.js"),
 };
