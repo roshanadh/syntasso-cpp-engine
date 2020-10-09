4f33c7510bd7be85f4e508cf6b397b968af14f0b
Roshan <nahsorad@gmail.com>
2020-10-09 17:47:34 +0545

    Export func to create sampleInputs and expectedOutputs dir

---
+++ filesystem/createTestFilesPath.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const path = require("path");
+
+module.exports = socketId => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			await require("./removeTestFiles.js")(socketId);
+			const sampleInputsDirPath = path.resolve(
+				__dirname,
+				"..",
+				"client-files",
+				socketId,
+				"sampleInputs"
+			);
+			const expectedOutputsDirPath = path.resolve(
+				__dirname,
+				"..",
+				"client-files",
+				socketId,
+				"expectedOutputs"
+			);
+			console.log(
+				`Creating test case files path for socket ID: ${socketId}...`
+			);
+			fs.mkdir(sampleInputsDirPath, { recursive: true }, err => {
+				if (err) {
+					// do nothing if path already exists
+					if (err.code === "EEXIST") {
+					} else {
+						reject(err);
+						throw err;
+					}
+				}
+				fs.mkdir(expectedOutputsDirPath, { recursive: true }, err => {
+					if (err) {
+						// do nothing if path already exists
+						if (err.code === "EEXIST") {
+						} else {
+							reject(err);
+							throw err;
+						}
+					}
+					console.log(
+						`Test case files path created for socket ID: ${socketId}.`
+					);
+					return resolve(true);
+				});
+			});
+		} catch (error) {
+			console.error(`Error inside createTestFilesPath:`, error);
+			return reject(error);
+		}
+	});
+};
