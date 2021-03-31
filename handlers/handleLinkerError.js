8515970f74fb128cf63041ddbee3396a8592b342
Roshan <nahsorad@gmail.com>
2021-03-31 13:32:44 +0545

    Invoke emitErrorBeforeExecEvent

--- handlers/handleLinkerError.js
+++ handlers/handleLinkerError.js
@@ -1,14 +1,30 @@
 const { linkerErrorParser, logger } = require("../util/index.js");
 
-module.exports = (req, res, next, times, linkerError, compilationWarnings) => {
+module.exports = (
+	req,
+	res,
+	next,
+	_socketInstance,
+	times,
+	linkerError,
+	compilationWarnings
+) => {
 	try {
 		const _parsedError = linkerErrorParser(linkerError);
 		if (_parsedError.errorInParser) return next(_parsedError.errorInParser);
 		const response = {
 			compilationWarnings,
 			error: { ..._parsedError, errorType: "linker-error" },
+			// send empty processes array
+			processes: [],
 			...times,
 		};
+
+		emitErrorBeforeExecEvent(
+			_socketInstance.socketId,
+			_socketInstance.socketInstance,
+			req.body.testCases.length
+		);
 		logger.info("Response to the client:", response);
 		return res.json(response);
 	} catch (error) {
