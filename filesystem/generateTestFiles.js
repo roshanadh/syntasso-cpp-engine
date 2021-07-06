fc67240c7c4321e4a3692cc28055a31f0443c6c7
Roshan <nahsorad@gmail.com>
2021-07-06 17:19:44 +0545

    Use async fs.writeFile calls from fs.promises module

--- filesystem/generateTestFiles.js
+++ filesystem/generateTestFiles.js
@@ -1,69 +1,73 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
 const { logger } = require("../util/index.js");
 
 module.exports = req => {
-	return new Promise((resolve, reject) => {
-		// at this point, both sampleInputs and expectedOutputs dirs have ...
-		// ... been created, so write files inside the directories
+	return new Promise(async (resolve, reject) => {
 		const { socketId, testCases } = req.body;
-		const sampleInputsDirPath = path.resolve(
-			__dirname,
-			"..",
-			"client-files",
-			socketId,
-			"sampleInputs"
-		);
-		const expectedOutputsDirPath = path.resolve(
-			__dirname,
-			"..",
-			"client-files",
-			socketId,
-			"expectedOutputs"
-		);
-		let sampleInputFilePath, expectedOutputFilePath;
 		try {
-			logger.info(
-				`Generating test case files for socket ID: ${socketId}...`
+			// create new test files directories
+			await require("./createTestFilesPath.js")(socketId);
+
+			const sampleInputsDirPath = path.resolve(
+				__dirname,
+				"..",
+				"client-files",
+				socketId,
+				"sampleInputs"
 			);
-			testCases.forEach((element, index) => {
-				sampleInputFilePath = path.resolve(
-					sampleInputsDirPath,
-					`${socketId}-sampleInput-${index}.txt`
-				);
-				expectedOutputFilePath = path.resolve(
-					expectedOutputsDirPath,
-					`${socketId}-expectedOutput-${index}.txt`
-				);
-				try {
-					fs.writeFileSync(
+			const expectedOutputsDirPath = path.resolve(
+				__dirname,
+				"..",
+				"client-files",
+				socketId,
+				"expectedOutputs"
+			);
+
+			logger.info(
+				`Generating test case files for socket ID ${socketId}...`
+			);
+			// at this point, both sampleInputs and expectedOutputs dirs have ...
+			// ... been created, so write files inside the directories
+			let sampleInputFilePath, expectedOutputFilePath;
+
+			await Promise.all(
+				testCases.map(async (element, index) => {
+					sampleInputFilePath = path.resolve(
+						sampleInputsDirPath,
+						`${socketId}-sampleInput-${index}.txt`
+					);
+					await fs.writeFile(
 						sampleInputFilePath,
 						element.sampleInput.toString()
 					);
 					logger.info(
 						`${socketId}-sampleInput-${index}.txt generated.`
 					);
-					fs.writeFileSync(
+				}),
+				testCases.map(async (element, index) => {
+					expectedOutputFilePath = path.resolve(
+						expectedOutputsDirPath,
+						`${socketId}-expectedOutput-${index}.txt`
+					);
+					await fs.writeFile(
 						expectedOutputFilePath,
 						element.expectedOutput.toString()
 					);
 					logger.info(
 						`${socketId}-expectedOutput-${index}.txt generated.`
 					);
-				} catch (error) {
-					logger.error(
-						`Error while writing to test case files for socketId ${socketId}:`,
-						error
-					);
-					return reject({ errorInGenerateTestFiles: error });
-				}
-			});
+				})
+			);
 			logger.info(`Test case files generated for socket ID ${socketId}.`);
 			return resolve(true);
 		} catch (error) {
-			logger.error(`Error in generateTestFiles:`, error);
-			return reject({ errorInGenerateTestFiles: error });
+			logger.error(
+				`Error while writing to test case files for socketId ${socketId}:`,
+				error
+			);
+			return reject(error);
 		}
 	});
 };
